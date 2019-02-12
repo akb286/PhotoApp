7a8032307051570cc44fb04461313f0e854366c9
Alicia Baker <aliciabaker314@gmail.com>
2019-02-11 18:53:11 -0700

    sendgrid setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,10 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { :host => 'http://localhost:3000/' }
+
+  config.action_mailer.perform_caching = false
 
   # Show full error reports.
   config.consider_all_requests_local = true
